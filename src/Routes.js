7f4cff481f24cbbb19eef644a30a161e894d033b
Stuart Bradshaw <stuartb@Hyperion.local>
2019-11-17 17:56:04 +0000

    Create a user sign up form

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,22 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute
+        path="/login"
+        exact component={Login}
+        appProps={appProps}
+      />
+      <AppliedRoute
+        path="/signup"
+        exact component={Signup}
+        appProps={appProps}
+      />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
