8eb94442a0fc26dee3ffdd20f6522fdadad5c4cb
Stuart Bradshaw <stuartb@Hyperion.local>
2019-11-19 23:35:42 +0000

    Add S3 file upload capacity for user

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { API } from "aws-amplify";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { s3Upload } from "../libs/awsLib";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./NewNote.css";
@@ -32,7 +33,11 @@ export default function NewNote(props) {
     setIsLoading(true);
   
     try {
-      await createNote({ content });
+      const attachment = file.current
+        ? await s3Upload(file.current)
+        : null;
+  
+      await createNote({ content, attachment });
       props.history.push("/");
     } catch (e) {
       alert(e);
